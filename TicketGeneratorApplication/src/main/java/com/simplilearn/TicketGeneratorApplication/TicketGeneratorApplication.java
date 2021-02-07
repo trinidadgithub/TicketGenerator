dce49c5f2cfa999b2e7e0dc3ce745c3f5cf493ec
student <trinidad.marroquin@gmail.com>
2021-02-07 02:26:54 +0000

    Add logic and test

---
+++ TicketGeneratorApplication/src/main/java/com/simplilearn/TicketGeneratorApplication/TicketGeneratorApplication.java
@@ -0,0 +1,20 @@
+package com.simplilearn.TicketGeneratorApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketGeneratorApplication {
+	public static double getRandomTicketNumber(){
+   	 double min=1000;
+   	 double max=9999;
+   		 double x = (int)(Math.random()*((max-min)+1))+min;
+   		 return x;
+    }
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketGeneratorApplication.class, args);
+	}
+
+}
---
+++ TicketGeneratorApplication/src/test/java/com/simplilearn/TicketGeneratorApplication/TicketGeneratorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.simplilearn.ticketGenerator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGeneratorApplicationTests {
+
+    private TicketGeneratorApplication ticketer = new TicketGeneratorApplication();
+    @Test	 
+    public void testRandomNumber() {     	 
+   	 assertTrue(ticketer.getRandomTicketNumber()>1000&&ticketer.getRandomTicketNumber()<9999);	 
+    }
+
+}
+
